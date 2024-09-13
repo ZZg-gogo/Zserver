0714a56e5ecfd888a2c3ad537d13d10ef8b9f3c1
zzh <1492658003@qq.com>
2024-09-13 22:55:38 +0800

    完成日志的append类

--- base/Logger.cpp
+++ base/Logger.cpp
@@ -1,1 +1,104 @@
 #include "Logger.h"
+
+#include <iostream>
+
+namespace BASE
+{
+
+
+Logger::Logger(const std::string &name) :
+    name_(name),
+    level_(LoggerLevel::INFO)
+{
+    
+}
+
+void Logger::addAppend(LoggerAppend::ptr append)
+{
+    appenders_.push_back(append);
+}
+
+void Logger::delAppend(LoggerAppend::ptr append)
+{
+    for (auto it = appenders_.begin(); appenders_.end() != it; ++it)
+    {
+        if (*it == append)
+        {
+            appenders_.erase(it);
+            break;
+        }
+        
+    }
+    
+}
+
+void Logger::log(LoggerLevel level, LoggerContent::ptr content)
+{
+    if (level < level_) //日志级别低的不输出
+    {
+        return;
+    }
+    
+    for (auto it = appenders_.begin(); appenders_.end() != it; ++it)
+    {
+        (*it)->log(level, content);
+    }
+    
+}
+void Logger::debug(LoggerContent::ptr content)
+{
+    log(LoggerLevel::DEBUG, content);
+}
+void Logger::info(LoggerContent::ptr content)
+{
+    log(LoggerLevel::INFO, content);
+}
+void Logger::warn(LoggerContent::ptr content)
+{
+    log(LoggerLevel::WARN, content);
+}
+void Logger::error(LoggerContent::ptr content)
+{
+    log(LoggerLevel::ERROR, content);
+}
+
+void Logger::fail(LoggerContent::ptr content)
+{
+    log(LoggerLevel::FAIL, content);
+}
+
+
+FileLogAppend::FileLogAppend(const std::string & filename) : 
+    filename_(filename)
+{
+
+}
+
+void FileLogAppend::log(LoggerLevel level, LoggerContent::ptr content)
+{
+    filestream_<<format_->format(content);
+}
+
+void FileLogAppend::reopen()
+{
+    if (filestream_)
+    {
+        filestream_.close();
+    }
+    
+    filestream_.open(filename_);
+}
+
+void StdoutLogAppend::log(LoggerLevel level, LoggerContent::ptr content)
+{
+    std::cout<<format_->format(content);
+}
+
+
+
+
+
+
+
+
+}
--- base/Logger.h
+++ base/Logger.h
@@ -3,6 +3,8 @@
 #include <time.h>
 #include <string>
 #include <memory>
+#include <list>
+#include <fstream>
 
 namespace BASE
 {
@@ -48,10 +50,13 @@ public:
     typedef std::shared_ptr<LoggerAppend> ptr;
 
 public:
-    void log(LoggerLevel level, LoggerContent::ptr content);
+    virtual void log(LoggerLevel level, LoggerContent::ptr content) = 0;
     virtual ~LoggerAppend() {}
-private:
-
+    //设置日志的输出格式
+    void setFormat(LoggerFormat::ptr format) {format_ = format;}
+    LoggerFormat::ptr getFormat() {return format_;}
+protected:
+    LoggerFormat::ptr format_;
 };
 
 class Logger    //日志器
@@ -63,21 +68,49 @@ public:
     Logger(const std::string &name = "Zlog");
 
     void log(LoggerLevel level, LoggerContent::ptr content);
+    void debug(LoggerContent::ptr content);
+    void info(LoggerContent::ptr content);
+    void warn(LoggerContent::ptr content);
+    void error(LoggerContent::ptr content);
+    void fail(LoggerContent::ptr content);
+
+    void addAppend(LoggerAppend::ptr append);
+    void delAppend(LoggerAppend::ptr append);
+
+    //设置获取等级
+    LoggerLevel getLevel() const {return level_;}
+    void setLevel(LoggerLevel level) {level_ = level;}
 private:
     std::string name_;
-    LoggerLevel level_;
-    LoggerAppend::ptr appender_;
+    LoggerLevel level_; //日志级别 日志级别大于level_才会被输出
+    std::list<LoggerAppend::ptr> appenders_;    //输出的目的地集合 
+     
 };
 
 
 class StdoutLogAppend : public LoggerAppend //输出到控制台
 {
+public:
+    typedef std::shared_ptr<StdoutLogAppend> ptr;
 
+public:
+    virtual void log(LoggerLevel level, LoggerContent::ptr content) override;
+
+private:
 };
 
 class FileLogAppend : public LoggerAppend   //输出到文件
 {
+public:
+    typedef std::shared_ptr<FileLogAppend> ptr;
 
+public:
+    FileLogAppend(const std::string & filename);    //输出到哪个文件
+    virtual void log(LoggerLevel level, LoggerContent::ptr content) override;
+    void reopen();  //需要打开文件
+private:
+    std::string filename_;  //文件名
+    std::ofstream filestream_;
 };
 
 }
