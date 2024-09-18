9e1b6df227bfe4578a4a5efa5c208eba4dd4ddf6
zhuzhenghua <plxzzh4224666>
2024-09-18 19:53:18 +0800

    完成LoggerFormat的init字符串解析

--- base/Logger.cpp
+++ base/Logger.cpp
@@ -2,6 +2,10 @@
 
 #include <sstream>
 #include <iostream>
+#include <tuple>
+#include <cctype>
+#include <map>
+#include <functional>
 
 namespace BASE
 {
@@ -23,6 +27,36 @@ static LoggerLevel2Str LoggerLevel2StrArr[] =
     {LoggerLevel::UNKONW, "UNKONW"}
 };
 
+static const char* LoggerLevel2StrFun(LoggerLevel level)
+{
+    for (int i = 0; i < sizeof(LoggerLevel2StrArr)/sizeof(LoggerLevel2Str); i++)
+    {
+        if (LoggerLevel2StrArr[i].level == level)
+        {
+            return LoggerLevel2StrArr[i].str;
+        }
+        
+    }
+    
+    return "UNKONW";
+}
+
+
+static LoggerLevel Str2LoggerLevelFun(const std::string& str)
+{
+    for (int i = 0; i < sizeof(LoggerLevel2StrArr)/sizeof(LoggerLevel2Str); i++)
+    {
+        if (LoggerLevel2StrArr[i].str == str)
+        {
+            return LoggerLevel2StrArr[i].level;
+        }
+        
+    }
+    
+    return LoggerLevel::UNKONW;
+}
+
+
 Logger::Logger(const std::string &name) :
     name_(name),
     level_(LoggerLevel::INFO)
@@ -41,10 +75,8 @@ void Logger::delAppend(LoggerAppend::ptr append)
         {
             appenders_.erase(it);
             break;
-        }
-        
-    }
-    
+        }  
+    } 
 }
 
 void Logger::log(LoggerLevel level, LoggerContent::ptr content)
@@ -110,14 +142,342 @@ void StdoutLogAppend::log(LoggerLevel level, LoggerContent::ptr content)
 }
 
 
+
+
+class  MessageFormatItem : public LoggerFormat::FormatItem
+{
+public:
+    MessageFormatItem(const std::string& str = ""){}
+
+    virtual void format(std::ostream& os, LoggerContent::ptr content) override
+    {
+        
+    }
+private:
+}
+
+class LevelFormatItem : public LoggerFormat::FormatItem
+{
+public:
+    LevelFormatItem(const std::string& str = ""){}
+
+    virtual void format(std::ostream& os, LoggerContent::ptr content) override
+    {
+        
+    }
+private:
+}
+
+
+class ElapseFormatItem : public LoggerFormat::FormatItem
+{
+public:
+    ElapseFormatItem(const std::string& str = ""){}
+
+    virtual void format(std::ostream& os, LoggerContent::ptr content) override
+    {
+        
+    }
+private:
+}
+
+class NameFormatItem : public LoggerFormat::FormatItem
+{
+public:
+    NameFormatItem(const std::string& str = ""){}
+
+    virtual void format(std::ostream& os, LoggerContent::ptr content) override
+    {
+        
+    }
+private:
+}
+
+class ThreadIdFormatItem : public LoggerFormat::FormatItem
+{
+public:
+    ThreadIdFormatItem(const std::string& str = ""){}
+
+    virtual void format(std::ostream& os, LoggerContent::ptr content) override
+    {
+        
+    }
+private:
+}
+
+class NewLineFormatItem : public LoggerFormat::FormatItem
+{
+public:
+    NewLineFormatItem(const std::string& str = ""){}
+
+    virtual void format(std::ostream& os, LoggerContent::ptr content) override
+    {
+        
+    }
+private:
+}
+
+class NewLineFormatItem : public LoggerFormat::FormatItem
+{
+public:
+    NewLineFormatItem(const std::string& str = ""){}
+
+    virtual void format(std::ostream& os, LoggerContent::ptr content) override
+    {
+        
+    }
+private:
+}
+
+class DateTimeFormatItem : public LoggerFormat::FormatItem
+{
+public:
+    DateTimeFormatItem(const std::string& str = "%Y-%m-%d %H:%M:%S") : 
+        format_(str)
+    {
+    }
+
+    virtual void format(std::ostream& os, LoggerContent::ptr content) override
+    {
+        
+    }
+private:
+    std::string format_;
+}
+
+class FilenameFormatItem : public LoggerFormat::FormatItem
+{
+public:
+    FilenameFormatItem(const std::string& str = ""){}
+
+    virtual void format(std::ostream& os, LoggerContent::ptr content) override
+    {
+        
+    }
+private:
+}
+
+class LineFormatItem : public LoggerFormat::FormatItem
+{
+public:
+    LineFormatItem(const std::string& str = ""){}
+
+    virtual void format(std::ostream& os, LoggerContent::ptr content) override
+    {
+        
+    }
+private:
+}
+
+class TabFormatItem : public LoggerFormat::FormatItem
+{
+public:
+    TabFormatItem(const std::string& str = ""){}
+
+    virtual void format(std::ostream& os, LoggerContent::ptr content) override
+    {
+        
+    }
+private:
+}
+
+class TabFormatItem : public LoggerFormat::FormatItem
+{
+public:
+    TabFormatItem(const std::string& str = ""){}
+
+    virtual void format(std::ostream& os, LoggerContent::ptr content) override
+    {
+        
+    }
+private:
+}
+
+
+class StringFormatItem : public LogFormatter::FormatItem {
+public:
+    StringFormatItem(const std::string& str) :
+        str_(str) 
+    {}
+
+    virtual void format(std::ostream& os, LoggerContent::ptr content) override 
+    {
+        os << m_string;
+    }
+private:
+    std::string str_;
+};
+
+class FiberIdFormatItem : public LoggerFormat::FormatItem
+{
+public:
+    FiberIdFormatItem(const std::string& str = ""){}
+
+    virtual void format(std::ostream& os, LoggerContent::ptr content) override
+    {
+        
+    }
+private:
+}
+
+
+class ThreadNameFormatItem : public LoggerFormat::FormatItem
+{
+public:
+    ThreadNameFormatItem(const std::string& str = ""){}
+
+    virtual void format(std::ostream& os, LoggerContent::ptr content) override
+    {
+        
+    }
+private:
+
+}
+
 LoggerFormat::LoggerFormat(const std::string& pattern) : 
     pattern_(pattern)
 {
     init();
 }
 
+//LogFormatter("%d{%Y-%m-%d %H:%M:%S}%T%t%T%N%T%F%T[%p]%T[%c]%T%f:%l%T%m%n"
 void LoggerFormat::init()
 {
+    //1格式化说明符(str) 2format 3type(0普通的字符串 1其余类型)
+    std::vector<std::tuple<std::string, std::string, int>> res;
+
+    size_t len = pattern_.length();
+
+    std::string originStr;  //直接输出的字符串
+    for (size_t i = 0; i < len; ++i)
+    {
+        if (pattern_[i] != '%') //不需要格式化字符串 直接原样输出
+        {
+            originStr.append(1, pattern_[i]);
+            continue;
+        }
+
+        if (i+1 < len && pattern_[i+1] == '%')  //处理%%的情况
+        {
+            originStr.append(1, '%');
+            continue;
+        }
+        
+        //此时pattern_[i] = '%' 
+        size_t n = i + 1;   //从下一个字符开始
+        bool isStart = false;   //是否开始进行fmt的解析标志
+        size_t formatStart = 0;//fmt的起始下标
+        std::string str;
+        std::string fmt;
+
+        while (n < len)
+        {
+            //没有进行{}的解析 并且当前不是字母也不是{}
+            if (!isStart && !std::isalpha(pattern_[n]) && pattern_[n] != '{' && pattern_[n] != '}')
+            {
+                str = pattern_.substr(i+1, n-i-1);
+                break;
+            }
+
+            if (!isStart)
+            {
+                if (pattern_[n] == '{') //开始进行fmt的解析了
+                {
+                    isStart = true;
+                    str = pattern_.substr(i+1, n-i-1);
+                    formatStart = n;
+                }  
+            }
+            else
+            {
+                if (pattern_[n] == '}') //fmt的解析结束了
+                {
+                    isStart = false;
+                    fmt = pattern_.substr(formatStart+1, n-formatStart-1);
+                    formatStart = n;
+                    ++n;    
+                    break;//防止跳到while循环的第一个if里面
+                }
+            }
+            
+            ++n;
+            if (n == len)
+            {
+                if (str.empty())    //%后面没有可以解析的内容了 直接把原始字符串输出了
+                {
+                    str = pattern_.substr(i+1);
+                }
+                
+            }
+        }//end while
+
+        if (isStart)    //解析开始了 但是在给定的字符串里面没找到}
+        {
+            std::cout<<"pattern_ parse error" <<pattern_<<" - "<<pattern_.substr(i)<<std::endl;
+            res.push_back(std::make_tuple("<<pattern_error>>", fmt, 0));
+        }
+        else
+        {
+            if (!originStr.empty()) //原始输出的字符串不是空的话
+            {
+                res.push_back(std::make_tuple(originStr, std::string(), 0));
+                originStr.clear();
+            }
+            
+            res.push_back(std::make_tuple(str, fmt, 1));
+            i = n-1;    //因为for循环中还要++i
+        }
+    }// end for
+
+    if (!originStr.empty()) //原始输出的字符串不是空的话
+    {
+        res.push_back(std::make_tuple(originStr, std::string(), 0));
+        originStr.clear();
+    }
+
+    //通过格式化说明符去new出来新的item
+    static std::map<std::string, std::function<FormatItem::ptr(const std::string& str)>> s_formatItems = {
+#define  ITEM(str, C) \
+        {#str, [](const std::string& fmt) { return FormatItem::ptr(new C(fmt));}}
+
+        ITEM(m, MessageFormatItem),           //m:消息
+        ITEM(p, LevelFormatItem),             //p:日志级别
+        ITEM(r, ElapseFormatItem),            //r:累计毫秒数
+        ITEM(c, NameFormatItem),              //c:日志名称
+        ITEM(t, ThreadIdFormatItem),          //t:线程id
+        ITEM(n, NewLineFormatItem),           //n:换行
+        ITEM(d, DateTimeFormatItem),          //d:时间
+        ITEM(f, FilenameFormatItem),          //f:文件名
+        ITEM(l, LineFormatItem),              //l:行号
+        ITEM(T, TabFormatItem),               //T:Tab
+        ITEM(F, FiberIdFormatItem),           //F:协程id
+        ITEM(N, ThreadNameFormatItem),        //N:线程名称
+
+#undef  ITEM
+    };
+    
+
+    for (auto &v : res)
+    {
+        if (std::get<2>(v) == 0)    //普通字符串的话
+        {
+            formats_.push_back(FormatItem::ptr(new StringFormatItem(std::get<0>(v))));
+        }
+        else
+        {
+            auto it = s_formatItems.find(std::get<0>(v));
+            if (s_formatItems.end() == it)
+            {
+                formats_.push_back(FormatItem::ptr(new StringFormatItem("<<error_format %" + std::get<0>(v) + ">>")));
+            }
+            else
+            {
+                formats_.push_back(it->second(std::get<1>(v)));
+            }
+            
+        }
+        
+    }
     
 }
 
@@ -134,7 +494,25 @@ std::string LoggerFormat::format(LoggerContent::ptr content)
 
 
 
-
-
-
+void LoggerContent::format(const char* fmt, ...)
+{
+    va_list al;
+    va_start(al, fmt);
+    format(fmt, al);
+    va_end(al);
 }
+
+
+void LoggerContent::format(const char* fmt, va_list al)
+{
+    char* buf = nullptr;
+    int len = vasprintf(&buf, fmt, al);
+    if(len != -1) 
+    {
+        content_ << std::string(buf, len);
+        free(buf);
+    }
+}
+
+
+}//end namespace
--- base/Logger.h
+++ base/Logger.h
@@ -6,24 +6,12 @@
 #include <list>
 #include <fstream>
 #include <vector>
+#include <sstream>
+#include <mutex>
+
 
 namespace BASE
 {
-
-class LoggerContent //日志内容
-{
-public:
-    typedef std::shared_ptr<LoggerContent> ptr;
-    LoggerContent(std::string &content);
-private:
-    const char * file_; //文件名
-    int line_;  //行号
-    int threadId_;  //线程id
-    int fiberId_;   //携程id
-    time_t time_;   //时间戳
-    std::string content_;   //内容
-};
-
 
 enum class LoggerLevel  //日志级别
 {
@@ -35,13 +23,51 @@ enum class LoggerLevel  //日志级别
     FAIL = 5
 };
 
+class Logger;
+
+class LoggerContent //日志内容
+{
+public:
+    typedef std::shared_ptr<LoggerContent> ptr;
+    LoggerContent(Logger::ptr logger, LoggerLevel level, const char * fileName,
+        int line, int threadId, int fiberId, time_t t, const std::string& threadName);
+
+    Logger::ptr getLogger() const {return logger_;}
+    LoggerLevel getLoggerLevel() const {return level_;}
+    const char * getFileName() const {return file_;}
+    int getLine() const {return line_;}
+    int getThreadId() const {return threadId_;}
+    int getFiberId() const {return fiberId_;}
+    int getTime() const {return time_;}
+    const std::string& getThreadName() const {return threadName_;}
+    std::string getContent() const {return content_.str();}
+    std::stringstream& getSS() { return content_;}
+
+    void format(const char* fmt, ...);
+    void format(const char* fmt, va_list al);
+
+private:
+    Logger::ptr logger_;    //日志器
+    LoggerLevel level_; //日志等级
+    const char * file_; //文件名
+    int line_;  //行号
+    int threadId_;  //线程id
+    int fiberId_;   //携程id
+    time_t time_;   //时间戳
+    std::string threadName_;//线程名
+    std::stringstream content_;   //内容
+};
+
+
+
+
 
 class LoggerFormat  //日志格式化
 {
 public:
     typedef std::shared_ptr<LoggerFormat> ptr;
 
-    class FormatItem//不同的子类输出不同的字段
+    class FormatItem    //不同的子类输出不同的字段
     {
     public:
         typedef std::shared_ptr<FormatItem> ptr;
@@ -52,7 +78,7 @@ public:
 public:
     LoggerFormat(const std::string& pattern);
 
-    std::string format(LoggerContent::ptr content);
+    std::string format(LoggerContent::ptr content); //日志内容经过格式化输出
 
 private:
     void init();    //去做用户输入的pattern的解析工作
@@ -83,7 +109,7 @@ public:
     typedef std::shared_ptr<Logger> ptr;
 
 public:
-    Logger(const std::string &name = "Zlog");
+    Logger(const std::string &name = "Zlog");   //日志器的名字
 
     void log(LoggerLevel level, LoggerContent::ptr content);
     void debug(LoggerContent::ptr content);
@@ -92,16 +118,27 @@ public:
     void error(LoggerContent::ptr content);
     void fail(LoggerContent::ptr content);
 
+    //增加/删除日志的输出地
     void addAppend(LoggerAppend::ptr append);
     void delAppend(LoggerAppend::ptr append);
 
     //设置获取等级
     LoggerLevel getLevel() const {return level_;}
     void setLevel(LoggerLevel level) {level_ = level;}
+
+    //返回日志器的名字
+    const std::string& getLoggerName() const {return name_;}
+
+    //设置日志格式器
+    void setFormatter(const std::string& val);
+    void setFormatter(LogFormatter::ptr formater);
+    LoggerFormat::ptr getFormatter();
 private:
     std::string name_;
     LoggerLevel level_; //日志级别 日志级别大于level_才会被输出
-    std::list<LoggerAppend::ptr> appenders_;    //输出的目的地集合 
+    std::list<LoggerAppend::ptr> appenders_;    //输出的目的地集合 日志可以被输出到多个地方
+    std::mutex mutex_;  //互斥锁
+    LoggerFormat::ptr formater_;    //日志格式器
      
 };
 
@@ -131,4 +168,4 @@ private:
     std::ofstream filestream_;
 };
 
-}
+}//end namespace
