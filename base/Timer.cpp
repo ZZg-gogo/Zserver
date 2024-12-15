e60c26e53a7976ccdd48fed82631009bccee1002
zzh <1492658003@qq.com>
2024-12-15 20:40:44 +0800

    定时器模块

--- base/Timer.cpp
+++ base/Timer.cpp
@@ -15,10 +15,39 @@ Timer::Timer(uint64_t ms, Callback cb, bool repeat, TimerManager * manager) :
 }
 
 
- TimerManager::TimerManager()
- {
+//取消一个定时器
+bool Timer::cancel()
+{
+    Mutex::Lock lock(manager_->mutex_);
+    auto it = manager_->timers_.find(shared_from_this());
+    manager_->timers_.erase(it);
 
- }
+    return true;
+}
+//刷新定时器的时间
+bool Timer::refresh()
+{
+    Mutex::Lock lock(manager_->mutex_);
+    auto it = manager_->timers_.find(shared_from_this());
+
+    if (it == manager_->timers_.end())
+    {
+        return false;
+    }
+    
+    manager_->timers_.erase(it);
+    callCbTime_ = GetTimestampMs() + ms_;
+    manager_->timers_.insert(shared_from_this());
+    return true;
+}
+
+
+
+ TimerManager::TimerManager()
+{
+
+}
+
 TimerManager::~TimerManager()
 {
 
@@ -41,7 +70,7 @@ Timer::ptr TimerManager::addTimer(uint64_t ms, Callback cb, bool repeat)
     return *it;
 }
 
-
+//条件定时器的辅助函数
 static void ConditionCallback(std::weak_ptr<void> weak, Timer::Callback cb)
 {
     if (weak.lock())
@@ -55,10 +84,67 @@ static void ConditionCallback(std::weak_ptr<void> weak, Timer::Callback cb)
 Timer::ptr TimerManager::addConditionTimer(uint64_t ms, Callback cb,
     std::weak_ptr<void> weak, bool repeat)
 {
-    Mutex::Lock lock(mutex_);
     return addTimer(ms, std::bind(ConditionCallback, weak, cb), repeat);
 }
 
+
+uint64_t TimerManager::getNextTime()
+{
+    Mutex::Lock lock(mutex_);
+
+    if (timers_.empty())
+    {
+        return  UINT64_MAX;
+    }
+    else
+    {
+        uint64_t now = GetTimestampMs();
+        uint64_t nextTime = timers_.begin()->get()->getNextCallbackTime();
+        if (nextTime <= now)    //不知道为什么时间晚了
+        {
+            return 0;   //立即执行
+        }
+        else
+        {
+            return nextTime - now;  //还需要等待这么长的时间
+        }
+    }
+}
+
+void TimerManager::getExpireCallback(std::vector<Callback> & cbs)
+{
+    Mutex::Lock lock(mutex_);
+
+    uint64_t now = GetTimestampMs();
+
+    std::vector<Timer::ptr> expireTimer;
+
+    auto it = timers_.begin(); 
+    for (; timers_.end() != it; ++it)
+    {
+        if ((*it)->getNextCallbackTime() <= now)
+        {
+            cbs.push_back((*it)->cb_);
+            expireTimer.push_back(*it);
+        } 
+        else
+        {
+            break;
+        }
+    }
+
+    timers_.erase(timers_.begin(), it);
+    
+    for (auto& i : expireTimer)
+    {
+        if (i->repeat_)
+        {
+            i->callCbTime_ = now + i->ms_;
+            timers_.insert(i);
+        }
+    }
+    
+}
 
 
 }
--- base/Timer.h
+++ base/Timer.h
@@ -3,6 +3,7 @@
 #include <functional>
 #include <memory>
 #include <set>
+#include <vector>
 
 #include "Lock.h"
 
@@ -38,8 +39,10 @@ private:
     {
         return callCbTime_;
     }
-
-
+    //取消一个定时器
+    bool cancel();
+    //刷新定时器的时间
+    bool refresh();
 
 private:
     uint64_t ms_;               //定时器的周期
@@ -67,7 +70,10 @@ public:
     //条件定时器 当weak的智能指针还存在的时候 那么才会执行定时器的回调
     Timer::ptr addConditionTimer(uint64_t ms, Callback cb,
         std::weak_ptr<void> weak, bool repeat = false);
-
+    //获取epoll要定时多长时间
+    uint64_t getNextTime();
+    //超时触发了 获取到那些需要执行回调的函数
+    void getExpireCallback(std::vector<Callback> & cbs);
 protected:
     //插入了一个到期时间最小的timer
     virtual void minTimerChange() = 0;
